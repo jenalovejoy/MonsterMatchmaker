36443b7a18db9a26099db91c4bde30f997c109f0
Dawson Murphy <37557289+AineDawson@users.noreply.github.com>
2020-02-10 10:35:37 -0800

    All the files for the server
    node files are too large so they are not included

---
+++ data.js
@@ -0,0 +1,15 @@
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const DataSchema = new Schema(
+  {
+      
+  },
+ // { timestamps: false }
+ {collection: 'Monsters'}
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema)
